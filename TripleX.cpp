faade9796711d2a8c971b14aa21c81e602d483e2
AbsorbedBarrel <47505192+AbsorbedBarrel@users.noreply.github.com>
2020-01-31 19:04:49 -0500

    Updated TripleX with Comments

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 int main()
 {
+    //Breif Synopsis of Story
     std::cout << "Welcome. This is TripleX. \nA Highly Evolved Security Software. \nYour Organization Has been comprimised by a set of Canadian Hackers. \nThey Have set up a complicated encryption code to take your files. \nCrack the code and free yourselves. My time is running out. Save yourself while you can. 32fj weflHF#TF !!ERROR!!\n\n\n                                          -The Ravager.";
     std::cout << "\n\n\n... ... ...\nYou contemplate with yourself. A prompt appears onscreen. \'Triple X, Please Enter the Code\'\nEnter the three numbers to continue.";
     
-    int a = 3;
-    std::cout << std::endl << a;
-
     return 0;
 }
