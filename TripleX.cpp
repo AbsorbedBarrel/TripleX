ee4e47c331d694625a2be3de879c7d860f07028b
AbsorbedBarrel <47505192+AbsorbedBarrel@users.noreply.github.com>
2020-02-01 18:35:12 -0500

    Updated Startup Text

--- TripleX.cpp
+++ TripleX.cpp
@@ -3,7 +3,18 @@ int main()
 {
     //Breif Synopsis of Story
     std::cout << "Welcome. This is TripleX. \nA Highly Evolved Security Software. \nYour Organization Has been comprimised by a set of Canadian Hackers. \nThey Have set up a complicated encryption code to take your files. \nCrack the code and free yourselves. My time is running out. Save yourself while you can. 32fj weflHF#TF !!ERROR!!\n\n\n                                          -The Ravager.";
-    std::cout << "\n\n\n... ... ...\nYou contemplate with yourself. A prompt appears onscreen. \'Triple X, Please Enter the Code\'\nEnter the three numbers to continue.";
-    
+    std::cout << "\n\n\n... ... ...\nYou contemplate with yourself. A prompt appears onscreen. \'Triple X, Please Enter the Code\'\nEnter the Code to continue.\n";
+    //Variable Test
+    //Declare 3 number Code
+    const int a = 3;
+    const int b = 7;
+    const int c = 12;
+    const int sum = a+b+c;
+    const int product = a*b*c;
+
+    //Print sum and product.
+    std::cout << "There are three numbers in the code";
+    std::cout << std::endl << "The codes add up to: " << sum;
+    std::cout << std::endl << "The codes multiply to: " << product;
     return 0;
 }
