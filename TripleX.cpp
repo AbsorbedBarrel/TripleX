48b38c27715a9220bef340880266f7deae3108a9
AbsorbedBarrel <47505192+AbsorbedBarrel@users.noreply.github.com>
2020-02-01 21:13:01 -0500

    Added improvements to win condition.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
-int main()
+
+void PrintIntroduction()
 {
     //Breif Synopsis of Story
-    std::cout << "Welcome. This is TripleX. \nA Highly Evolved Security Software. \nYour organization Has been comprimised by a set of Canadian Hackers. \nThey Have set up a complicated encryption code to take your files and have begun to expose your database. \nCrack the code and free yourselves and save the database. My time is running out. Save yourself while you can. 32fj weflHF#TF !!ERROR!!\n\n\n                                          -The Ravager.";
-    std::cout << "\n\n\n... ... ...\nYou contemplate with yourself. A prompt appears onscreen. \'Triple X, Please Enter the Code\'\nEnter the Code to continue.\n";
+    std::cout << "\n\nWelcome. This is TripleX. \nA Highly Evolved Security Software. \nYour organization Has been comprimised by a set of Canadian Hackers. \nThey Have set up a complicated encryption code to take your files and have begun to expose your database. \nCrack the code and free yourselves and save the database. My time is running out. Save yourself while you can. 32fj weflHF#TF !!ERROR!!\n\n\n                                          -The Ravager.";
+    std::cout << "\n\n\n ";
+    std::cout << "   ||||||||||||||||||||||||||||||" << std::endl;
+    std::cout << "    ||||----------------------||||" << std::endl;
+    std::cout << "    ||||*                    *||||" << std::endl;
+    std::cout << "    ||||*                    *||||" << std::endl;
+    std::cout << "    ||||*      TripleX       *||||" << std::endl;
+    std::cout << "    ||||*                    *||||" << std::endl;
+	std::cout << "    ||||*    ___________     *||||" << std::endl;
+	std::cout << "    ||||*                    *||||" << std::endl;
+	std::cout << "    ||||----------------------||||" << std::endl;
+	std::cout << "    ||||||||||||||||||||||||||||||" << std::endl;
+    std::cout << "|||||||||||||||||||||||||||||||||||||||" << std::endl;
+	std::cout << "|||||||||||||||||||||||||||||||_____|||" << std::endl;
+	std::cout << "|||||||||||||||||||||||||||||//     \\\\||" << std::endl;
+    std::cout << "||||---------------------|||||| (*) ||||" << std::endl;
+    std::cout << "|||||||||||||||||||||||||||||\\\\_____//||" << std::endl;
+    std::cout << "\n... ... ...\nYou contemplate with yourself. A prompt appears onscreen. \'Triple X, Please Enter the Code\'\nEnter the Code to continue.\n";
+    return;
+}
 
+bool PlayGame()
+{
     const int CodeA = 3;
     const int CodeB = 7;
     const int CodeC = 12;
@@ -13,24 +34,39 @@ int main()
 
     //Print CodeSum and CodeProduct to the terminal.
     std::cout << "\nX There are three numbers in the code";
-    std::cout << std::endl << "X The codes add up to: " << CodeSum;
-    std::cout << std::endl << "X The codes multiply to: " << CodeProduct << std::endl;
-
+    std::cout << "\nX The codes add up to: " << CodeSum;
+    std::cout << "\nX The codes multiply to: " << CodeProduct << std::endl;
+    //Store Player Guess
     int GuessA, GuessB, GuessC;
-
-    std::cout << std::endl << "Enter Guess: ";
+    std::cout << "\nEnter Guess: ";
     std::cin >> GuessA;
     std::cin >> GuessB;
     std::cin >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
+
+    //Check if the players guess is correct
     if(GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
         std::cout << "Congratulations! You've broken through the encryption and saved your orgnization!";
+        return true;
     }else 
     {
         std::cout << "!!ERROR!! INVALID INPUT\nYou've failed to crack the code in time. Your files have been encrypted for ransom and the database has been exposed.";
+        return false;
+    }
+}
+
+int main()
+{
+    PrintIntroduction();
+    while(true)
+    {
+        bool bLevelComplete = PlayGame();
+        std::cin.clear();//Clears Errors
+        std::cin.ignore();//Discards Buffer
     }
     return 0;
 }
+
